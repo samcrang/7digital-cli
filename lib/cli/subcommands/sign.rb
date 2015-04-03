17e9e83ee93f9257a25845b563eaa921937a4ffd
Sam Crang <sam.crang@gmail.com>
2015-04-04 00:11:03 +0100

    Tidy up

--- lib/cli/subcommands/sign.rb
+++ lib/cli/subcommands/sign.rb
@@ -10,22 +10,32 @@ module SevenDigital
     class Sign < Thor
       desc 'track_details', 'track/details'
       def track_details(track_id)
-        puts ::SevenDigital::Endpoints::TrackDetails.new(ENV['SEVENDIGITAL_CONSUMER_KEY']).generate_url(track_id: track_id)
+        puts ::SevenDigital::Endpoints::TrackDetails.new(
+          ENV['SEVENDIGITAL_CONSUMER_KEY']
+        ).generate_url(track_id: track_id)
       end
 
       desc 'release_details', 'release/details'
       def release_details(release_id)
-        puts ::SevenDigital::Endpoints::ReleaseDetails.new(ENV['SEVENDIGITAL_CONSUMER_KEY']).generate_url(release_id: release_id)
+        puts ::SevenDigital::Endpoints::ReleaseDetails.new(
+          ENV['SEVENDIGITAL_CONSUMER_KEY']
+        ).generate_url(release_id: release_id)
       end
 
       desc 'clip', 'clip'
       def clip(track_id)
-        puts ::SevenDigital::Endpoints::Clip.new(ENV['SEVENDIGITAL_CONSUMER_KEY'], ENV['SEVENDIGITAL_CONSUMER_SECRET']).generate_url(track_id: track_id, country: 'GB')
+        puts ::SevenDigital::Endpoints::Clip.new(
+          ENV['SEVENDIGITAL_CONSUMER_KEY'],
+          ENV['SEVENDIGITAL_CONSUMER_SECRET']
+        ).generate_url(track_id: track_id, country: 'GB')
       end
 
       desc 'stream_catalogue', 'stream/catalogue'
       def stream_catalogue(track_id)
-        puts ::SevenDigital::Endpoints::StreamCatalogue.new(ENV['SEVENDIGITAL_CONSUMER_KEY'], ENV['SEVENDIGITAL_CONSUMER_SECRET']).generate_url(track_id: track_id)
+        puts ::SevenDigital::Endpoints::StreamCatalogue.new(
+          ENV['SEVENDIGITAL_CONSUMER_KEY'],
+          ENV['SEVENDIGITAL_CONSUMER_SECRET']
+        ).generate_url(track_id: track_id)
       end
 
       desc 'stream_subscription', 'stream/subscription'
