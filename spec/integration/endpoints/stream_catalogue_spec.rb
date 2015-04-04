04dadb29854aa455f9c689e4a8d701410021db0e
Sam Crang <sam.crang@gmail.com>
2015-04-04 17:19:10 +0100

    Forgot to run rubocop :(

--- spec/integration/endpoints/stream_catalogue_spec.rb
+++ spec/integration/endpoints/stream_catalogue_spec.rb
@@ -4,7 +4,7 @@ describe 'StreamCatalogue' do
   subject { ::SevenDigital::Endpoints::StreamCatalogue.new(ENV['SEVENDIGITAL_CONSUMER_KEY'], ENV['SEVENDIGITAL_CONSUMER_SECRET']) }
 
   describe '#generate_url' do
-    it 'should return a signed url', :skip => using_demo_key? do
+    it 'should return a signed url', skip: using_demo_key? do
       uri = URI(subject.generate_url(track_id: 1234))
 
       res = Net::HTTP.get_response(uri)
--- spec/integration/endpoints/stream_subscription_spec.rb
+++ spec/integration/endpoints/stream_subscription_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe 'StreamSubscription' do
-  subject { ::SevenDigital::Endpoints::StreamSubscription.new(
-    ENV['SEVENDIGITAL_CONSUMER_KEY'],
-    ENV['SEVENDIGITAL_CONSUMER_SECRET'],
-    ENV['SEVENDIGITAL_TOKEN'],
-    ENV['SEVENDIGITAL_TOKEN_SECRET']
-  ) }
+  subject do
+    ::SevenDigital::Endpoints::StreamSubscription.new(
+      ENV['SEVENDIGITAL_CONSUMER_KEY'],
+      ENV['SEVENDIGITAL_CONSUMER_SECRET'],
+      ENV['SEVENDIGITAL_TOKEN'],
+      ENV['SEVENDIGITAL_TOKEN_SECRET']
+    )
+  end
 
   describe '#generate_url' do
-    it 'should return a signed url', :skip => using_demo_key? do
+    it 'should return a signed url', skip: using_demo_key? do
       uri = URI(subject.generate_url(track_id: 1234))
 
       res = Net::HTTP.get_response(uri)
