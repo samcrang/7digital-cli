5d61fe4297e18550af853e1534d615d67599196c
Sam Crang <sam.crang@gmail.com>
2015-04-05 21:48:29 +0100

    Add additional test coverage

--- spec/cli/app_spec.rb
+++ spec/cli/app_spec.rb
@@ -19,18 +19,24 @@ describe ::SevenDigital::CLI::App do
 
     context 'clip' do
       describe '#run' do
+        before(:each) do
+          allow(factory).to receive(:find).with('clip').and_return(signature_generator)
+        end
+
         it 'should pass the correct arguments to the endpoint handler' do
-          expect(factory).to receive(:find).with('clip').and_return(signature_generator)
           expect(signature_generator).to receive(:generate_url).with(1234, 'GB')
 
           subject.run(%w(sign clip --trackid=1234))
         end
 
         it 'should allow country to be overridden' do
-          expect(factory).to receive(:find).with('clip').and_return(signature_generator)
           expect(signature_generator).to receive(:generate_url).with(1234, 'US')
 
           subject.run(%w(sign clip --trackid=1234 --country=US))
+        end
+
+        it 'should not allow someone to specify a non-integer track id' do
+          expect { subject.run(%w(sign clip --trackid=invalid)) }.to raise_error(SystemExit)
         end
       end
     end
